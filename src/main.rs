cfb3356e6b21bcba5b866ec81b600c9cf584859b
Guillaume Fgt <guillaume.faguet@protonmail.com>
2025-03-23 09:33:38 +0100

    extracted data creation into a separate func

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,6 @@ mod data;
 mod frame;
 
 fn main() -> eyre::Result<()> {
-    let stdin = std::io::stdin();
-    let mut stdin = stdin.lock();
-
     let Opt {
         log_x,
         log_y,
@@ -26,76 +23,7 @@ fn main() -> eyre::Result<()> {
         flip,
     } = Opt::parse_from_env().context("parse command-line arguments")?;
 
-    let mut data = Data::default();
-    let mut canvas = Canvas::new(height, width, mode);
-
-    let mut line = String::new();
-    for row in 0.. {
-        line.clear();
-        let mut x = x_is_row.then_some(row as f64);
-        let n = stdin.read_line(&mut line).context("read input line")?;
-        if n == 0 {
-            break;
-        }
-
-        let mut column = 0;
-        let mut line = line.trim_end();
-        while !line.is_empty() {
-            let (next_num, remainder) = line
-                .split_once(|c| !matches!(c, '0'..='9' | '-' | '+' | '.' | 'E' | 'e'))
-                .unwrap_or((line, ""));
-            line = remainder;
-            let v = match next_num.parse::<f64>() {
-                Ok(v) => {
-                    assert!(!v.is_infinite(), "{v}");
-                    v
-                }
-                Err(_) => {
-                    // invalid values are treated as missing
-                    f64::NAN
-                }
-            };
-            if x.is_some() {
-                // have a data point!
-                if column + 1 > data.ys.len() {
-                    if column >= MARKS.len() {
-                        // we can't label these ones!
-                        continue;
-                    }
-                    assert_eq!(
-                        column,
-                        data.ys.len(),
-                        "we will only ever add a single new column at a time"
-                    );
-                    // discovered a new column!
-                    // need to add the column, which means adding empty
-                    // values for that column for all pre-existing rows.
-                    data.ys.push(vec![f64::NAN; data.xs.len()]);
-                }
-                data.ys[column].push(v);
-                column += 1;
-            } else {
-                // found x value
-                x = Some(v);
-            }
-        }
-
-        // whatever x value we discovered is the x for the row
-        // NOTE: if this is None, that means there were no column values at all, which is
-        // equivalent to an empty line, which we simply don't count as a sample. note also that
-        // this means ys has not been pushed to either.
-        if let Some(x) = x {
-            data.xs.push(x);
-        }
-
-        // make sure we fill in the other column values
-        for y in &mut data.ys {
-            if y.len() < data.xs.len() {
-                assert_eq!(y.len(), data.xs.len() - 1);
-                y.push(f64::NAN);
-            }
-        }
-    }
+    let mut data = create_data(&x_is_row)?;
 
     if log_x {
         for x in &mut data.xs {
@@ -152,6 +80,7 @@ fn main() -> eyre::Result<()> {
         frame = Frame::new_over(width, height, &data);
     }
 
+    let mut canvas = Canvas::new(height, width, mode);
     frame.draw_into(&mut canvas);
     data.draw_into(&mut canvas, &frame);
 
@@ -198,4 +127,77 @@ fn render(
     writeln!(out)?;
     writeln!(out, "{canvas}")?;
     Ok(())
+}
+
+fn create_data(x_is_row: &bool) -> eyre::Result<Data> {
+    let stdin = std::io::stdin();
+    let mut stdin = stdin.lock();
+    let mut data = Data::default();
+    let mut line = String::new();
+    for row in 0.. {
+        line.clear();
+        let mut x = x_is_row.then_some(row as f64);
+        let n = stdin.read_line(&mut line).context("read input line")?;
+        if n == 0 {
+            break;
+        }
+
+        let mut column = 0;
+        let mut line = line.trim_end();
+        while !line.is_empty() {
+            let (next_num, remainder) = line
+                .split_once(|c| !matches!(c, '0'..='9' | '-' | '+' | '.' | 'E' | 'e'))
+                .unwrap_or((line, ""));
+            line = remainder;
+            let v = match next_num.parse::<f64>() {
+                Ok(v) => {
+                    assert!(!v.is_infinite(), "{v}");
+                    v
+                }
+                Err(_) => {
+                    // invalid values are treated as missing
+                    f64::NAN
+                }
+            };
+            if x.is_some() {
+                // have a data point!
+                if column + 1 > data.ys.len() {
+                    if column >= MARKS.len() {
+                        // we can't label these ones!
+                        continue;
+                    }
+                    assert_eq!(
+                        column,
+                        data.ys.len(),
+                        "we will only ever add a single new column at a time"
+                    );
+                    // discovered a new column!
+                    // need to add the column, which means adding empty
+                    // values for that column for all pre-existing rows.
+                    data.ys.push(vec![f64::NAN; data.xs.len()]);
+                }
+                data.ys[column].push(v);
+                column += 1;
+            } else {
+                // found x value
+                x = Some(v);
+            }
+        }
+        // whatever x value we discovered is the x for the row
+        // NOTE: if this is None, that means there were no column values at all, which is
+        // equivalent to an empty line, which we simply don't count as a sample. note also that
+        // this means ys has not been pushed to either.
+        if let Some(x) = x {
+            data.xs.push(x);
+        }
+
+        // make sure we fill in the other column values
+        for y in &mut data.ys {
+            if y.len() < data.xs.len() {
+                assert_eq!(y.len(), data.xs.len() - 1);
+                y.push(f64::NAN);
+            }
+        }
+    }
+    Ok(data)
 }
